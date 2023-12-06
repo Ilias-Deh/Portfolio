fd125638a5b176b339c705d864a1e1609d9b0d16
Ilias Dehaynin <ilias.dehaynin@gmail.com>
2023-12-06 15:34:59 +0100

    disposition + couleur

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -5,12 +5,14 @@ function About() {
   return (
     <div id="about" role="main" >
       <h1>Dehaynin Ilias / Intégrateur Web</h1>
+      <div className="about-background">
       <div className="display-about">
       <img src={photo} alt="Ilias Dehaynin" className="about-picture"/>
       <div>
         <p>Salut, je suis Ilias</p>
         <p>Je suis Intégrateur Web, basé en France</p>
-        <p className="about-text">Travailleur et passioné par l'informatique<br/> je suis la personne qui vous faut pour développer vos idées en créant le site web qui vous correspond </p>
+        <p className="about-text">Travailleur et passioné par l'informatique<br/> je suis la personne qui vous faut pour développer vos idées<br/> en créant le site web qui vous correspond </p>
+      </div>
       </div>
       </div>
       <div className="display-aboutSec">
